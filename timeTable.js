566af1f94b138353537b09dba9bd63ef1933c13f
Nguyễn Huy Hoàng <115699781+hoanghelloworld@users.noreply.github.com>
2024-04-08 13:31:04 +0700

    Add files via upload

---
+++ timeTable.js
@@ -0,0 +1,133 @@
+const Sunday = [
+    {
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday = [
+    {
+        time: '09-10 AM',
+        roomNumber: '38-718',
+        subject: 'Cơ sở dữ liệu',
+        type: 'bài học'
+    },
+    {
+        time: '10-11 AM',
+        roomNumber: '38-718',
+        subject: 'Tư tưởng Hồ Chí Minh',
+        type: 'Hướng dẫn'
+    },
+    {
+        time: '12-01 PM',
+        roomNumber: '38-718',
+        subject: 'Kinh Tế chính trị',
+        type: 'bài học'
+    }
+]
+const Tuesday = [
+    {
+        time: '09-10 AM',
+        roomNumber: '27-304Y',
+        subject: 'Kinh Tế chính trị',
+        type: 'Hướng dẫn'
+    },
+    {
+        time: '11-12 AM',
+        roomNumber: '28-107',
+        subject: 'CS849',
+        type: 'bài học'
+    },
+    {
+        time: '12-01 PM',
+        roomNumber: '28-107',
+        subject: 'CS849',
+        type: 'bài học'
+    },
+    {
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'bài học'
+    }
+]
+
+const Wednesday = [
+    {
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'Triết học',
+        type: 'bài học'
+    },
+    {
+        time: '11-12 AM',
+        roomNumber: '38-719',
+        subject: 'CS200',
+        type: 'bài học'
+    }
+]
+
+const Thursday = [
+    {
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'Kinh Tế chính trị',
+        type: 'bài học'
+    },
+    {
+        time: '01-02 PM',
+        roomNumber: '38-719',
+        subject: 'CS849',
+        type: 'bài học'
+    },
+    {
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'bài học'
+    }
+]
+
+const Friday = [
+    {
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'MEC103',
+        type: 'bài học'
+    },
+    {
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'MEC103',
+        type: 'bài học'
+    },
+    {
+        time: '02-03 PM',
+        roomNumber: '33-601',
+        subject: 'CS849',
+        type: 'Hướng dẫn'
+    },
+
+]
+
+const Saturday = [
+    {
+        time: '09-10 AM',
+        roomNumber: '34-604',
+        subject: 'Triết học',
+        type: 'Hướng dẫn'
+    },
+    {
+        time: '10-11 AM',
+        roomNumber: '34-604',
+        subject: 'Triết học',
+        type: 'bài học'
+    },
+    {
+        time: '01-02 PM',
+        roomNumber: '33-309',
+        subject: 'Kinh Tế chính trị',
+        type: 'bài học'
+    }
+]
