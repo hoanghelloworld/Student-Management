f92153a3f6dda2e7b1da3806a728e0f02ca063ae
Trình Nguyễn <144087649+trinhdz-dz@users.noreply.github.com>
2024-04-09 10:39:55 +0700

    tamthoi

---
+++ menu.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var menu = document.getElementById("myMenu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
