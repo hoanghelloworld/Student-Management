823785cb85bf535911134b29c1d116acf266696a
quankaka11 <144087570+quankaka11@users.noreply.github.com>
2024-04-28 01:16:05 +0700

    thêm sự kiện menu.js

--- menu.js
+++ menu.js
@@ -13,7 +13,7 @@ document.getElementById('logout').addEventListener('click', function(event) {
   // Ngăn chặn hành vi mặc định của nút
   event.preventDefault();
   // Ẩn menu
-  toggleMenu();
+  window.location.href = "index.html";
 });
 
 function openContactBox() {
@@ -27,29 +27,43 @@ function closeContactBox() {
   contactBox.style.display = "none";
 }
 
-document.querySelector('form').addEventListener('submit', function(event) {
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyzK1YPSNayNYfX7GvNmFuR18vcUWy70anrUzlgJJQK_FNPr0qjIOAn7CUcfYtDZm1gFw/exec'
+const form = document.forms['contact-form']
+
+// Lấy giá trị email từ Local Storage
+var storedEmail = localStorage.getItem('username');
+
+// Gán giá trị email vào trường input ẩn
+document.getElementById('emailInput').value = storedEmail;
+
+form.addEventListener('submit', function(event) {
   event.preventDefault(); // Ngăn chặn hành vi mặc định của form
 
-  // Kiểm tra xem form có hợp lệ hay không
+  // Kiểm tra xem form có hợp lệ hay không  
   var isValid = validateForm();
 
   if (isValid) {
     // Thực hiện các hành động khi form hợp lệ
     // Đóng box
-    closeContactBox();
-    
-    // Gán input text bằng rỗng
-    document.querySelector('input[type="text"]').value = '';
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      // Gán input text bằng rỗng
+      document.querySelector('input[type="text"]').value = '';
+      document.getElementById('pointInput').value = '';
+      closeContactBox();
 
-    // Hiển thị thông báo gửi thành công
-    document.getElementById('success-message').style.display = 'block';
-    
-    // Tự động ẩn thông báo sau 3 giây
-    setTimeout(function() {
-      document.getElementById('success-message').style.display = 'none';
-    }, 3000);
+      // Hiển thị thông báo gửi thành công
+      document.getElementById('success-message').style.display = 'block';
+      
+      // Tự động ẩn thông báo sau 3 giây
+      setTimeout(function() {
+        document.getElementById('success-message').style.display = 'none';
+      }, 3000);
+    })
+    .catch(error => console.error('Error:', error));
   }
 });
+
 
 // Hàm kiểm tra hợp lệ của form
 function validateForm() {
