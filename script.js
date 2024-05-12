77bd6c2db659cf19c760984669a2d2f68f8d57f6
Trình Nguyễn <144087649+trinhdz-dz@users.noreply.github.com>
2024-05-12 16:01:50 +0000

    update faceid

--- script.js
+++ script.js
@@ -1,31 +1,74 @@
-const video = document.getElementById('video')
+const video = document.getElementById('video');
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models') //heavier/accurate version of tiny face detector
+]).then(start)
 
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+function start() {
+    document.body.append('Models Loaded')
+
+    // Lắng nghe sự kiện khi video bắt đầu phát
+    video.addEventListener('play', async () => {
+        console.log('Video is playing');
+        recognizeFaces(); // Gọi hàm nhận diện khuôn mặt khi video bắt đầu phát
+    });
+
+    // Mở luồng video từ webcam
+    navigator.mediaDevices.getUserMedia({ video: {} })
+        .then(function (stream) {
+            video.srcObject = stream;
+        })
+        .catch(function (err) {
+            console.error("Error accessing the camera: " + err);
+        });
 }
 
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
+async function recognizeFaces() {
+    const labeledDescriptors = await loadLabeledImages();
+    console.log(labeledDescriptors);
+    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.7);
+
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
+
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors();
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+        const results = resizedDetections.map((d) => {
+            return faceMatcher.findBestMatch(d.descriptor);
+        });
+        results.forEach((result, i) => {
+            const box = resizedDetections[i].detection.box;
+            const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
+            drawBox.draw(canvas);
+        });
+    }, 100);
+}
+
+function loadLabeledImages() {
+    // const labels = ['Quan']
+    const labels = ['Prashant Kumar','Quan','Thor','Trinhdz','Giang'] // for WebCam
+    document.body.append(labels + ' Faces Loading: \n');
+    return Promise.all(
+        labels.map(async (label) => {
+            const descriptions = [];
+            for (let i = 1; i <= 2; i++) {
+                const img = await faceapi.fetchImage(`../labeled_images/${label}/${i}.jpg`);
+                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+                console.log(label + i + JSON.stringify(detections));
+                descriptions.push(detections.descriptor);
+            }
+            document.body.append(label + ' Faces Loaded | ');
+            return new faceapi.LabeledFaceDescriptors(label, descriptions);
+        })
+    );
+}
